57ba6f06263a61e342856bb6c5a2d458eaf6a1e2
mika1407 <koneautomaatio@hotmail.com>
2019-12-28 07:05:08 +0200

    respond

--- main.py
+++ main.py
@@ -5,14 +5,22 @@ r = sr.Recognizer()
 
 def record_audio():
     with sr.Microphone() as source:
-        print('Say something')
         audio = r.listen(source)
         voice_data = ''
         try:
             voice_data = r.recognize_google(audio)
-            print(voice_data)
         except sr.UnknownValueError:
             print('Sorry, I did not get that')
         except sr.RequestError:
             print('Sorry, my speech service is down')
         return voice_data
+
+
+def respond(voice_data):
+    if 'what is your name' in voice_data:
+        print('My name is Alexis')
+
+
+print('How can I help you?')
+voice_data = record_audio()
+respond(voice_data)
